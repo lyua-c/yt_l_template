d4f86e27db974bca5c6ff6418b4539b3e15410be
ebina <ebina@monikas.tech>
2021-01-28 10:14:33 +0900

    なぜか保存に失敗するけど

--- scripts/apply.js
+++ scripts/apply.js
@@ -9,21 +9,26 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
   var elemTitle, elemBody, elemTag;
     // when PR
-    elemTitle = document.getElementsByClassName('video-settings-title')[0];
-    elemBody = document.getElementsByClassName('video-settings-description')[0];
+    var tmp_elemTitle = document.getElementsByClassName('input-container title')[0]
+    elemTitle = tmp_elemTitle.querySelector('#textbox');
+
+    var tmp_elemBody = document.getElementsByClassName('input-container description')[0];
+    elemBody = tmp_elemBody.querySelector('#textbox');
     if (document.getElementsByClassName('video-settings-tags') != null) {
      elemTag = document.getElementsByClassName('video-settings-tags')[0];
     }
 
   if (elemTitle && elemBody) {
-    elemTitle.value = args.title;
-    elemBody.value = args.body;
+    elemTitle.innerHTML = args.title;
+    elemTitle.dispatchEvent(new Event("onpaste"));
+    elemBody.innerHTML = args.body;
+    elemBody.dispatchEvent(new Event("onpaste"));
   }
-  if (document.getElementsByClassName('save-changes-button')[0] != null) {
-   document.getElementsByClassName('save-changes-button')[0].disabled = false
+  if (document.querySelector('#save-button')[0] != null) {
+   document.querySelector('#save-changes-button')[0].disabled = false
   }
-  if (document.getElementsByClassName('save-changes-button')[1] != null) {
-   document.getElementsByClassName('save-changes-button')[1].disabled = false
+  if (document.querySelector('#save-button')[1] != null) {
+   document.querySelector('#save-changes-button')[1].disabled = false
   }
   if (document.getElementsByClassName('yt-chip') != null) {
    while (document.getElementsByClassName('yt-chip')[0]) document.getElementsByClassName('yt-chip')[0].parentNode.removeChild(document.getElementsByClassName('yt-chip')[0]);
--- scripts/fetch.js
+++ scripts/fetch.js
@@ -12,11 +12,25 @@
 
 var title, body, tag;
 
-  title = document.getElementsByClassName('video-settings-title')[0].value;
-  body = document.getElementsByClassName('video-settings-description')[0].value;
-    if (document.getElementsByClassName('video-settings-tags')[0] != null) {
-     tag = document.getElementsByClassName('video-settings-tags')[0].value;
+  var title_tmp = document.getElementsByClassName('input-container title')[0];
+  //console.log(title_tmp);
+  console.log(title_tmp.querySelector('#textbox').innerHTML);
+  
+  title = title_tmp.querySelector('#textbox').innerHTML;
+  var body_tmp = document.getElementsByClassName('input-container description')[0];
+  console.log(body_tmp.querySelector('#textbox').innerHTML);
+  body = body_tmp.querySelector('#textbox').innerHTML
+
+  //console.log(document.querySelector('#tags-container'));
+  var tag_tmp = document.querySelector('#tags-container');
+  tag_tmp = tag_tmp.querySelector('#child-input')
+  var tag_tmp2 = tag_tmp.querySelector('#chip-bar');
+  console.log(tag_tmp2);
+  if (tag_tmp != null) {
+     //tag = document.querySelector('tags-container')[0].value;
+
     }
+
 var data = {
   "title": title,
   "body": body,
